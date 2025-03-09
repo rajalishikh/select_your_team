8795a349527afdb3df7cfe03cd781e59c1cb8382
rajalishikhe <nahidujjamanraj@gmail.com>
2025-03-09 10:21:00 +0600

    My work is complete for baner section

--- src/Component/Main/Main.jsx
+++ src/Component/Main/Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import baner from "../../assets/img/banner-main.png";
+import Ba from "../../assets/img/banner-main.png";
 import background from "../../assets/img/bg-shadow.png";
 
 const Main = () => {
@@ -10,7 +10,7 @@ const Main = () => {
   style={{ backgroundImage: `url(${background})` }}
 >
   <div className="flex flex-col items-center justify-center text-center  p-4">
-    <img src={baner} alt="Banner" className="mb-4" />
+    <img src={Ba} alt="Banner" className="mb-4" />
     <h2 className="text-[#FFFFFF] font-bold sm:text-xl md:text-2xl lg:text-4xl">Assemble Your Ultimate Dream 11 Cricket Team</h2>
     <p className="text-[#FFFFFFB3]">Beyond Boundaries Beyond Limits</p>
     <div className='border border-[#E7FE29] rounded-xl'>
